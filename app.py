8a5e836bb6d1490557a2fdb30a30de04e91485d7
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-01-20 21:58:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,6 @@ def main():
     
     img_logo = Image.open(os.path.join('data/images', os.path.basename("smartathonLogo.png")))
     st.image(img_logo, caption='Smartathon')
-    st.image(
     st.header('📦Obstacle Detection')
     st.subheader('👈🏽 Select options left-haned menu bar.')
     st.sidebar.markdown("https://smartathon.hackerearth.com/challenges/hackathon/smartathon-2/dashboard/ae23dcf/team/?utm_source=team-invite&utm_medium=email&utm_campaign=hackathons")
