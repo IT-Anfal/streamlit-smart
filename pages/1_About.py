122c6bdc0f30ba1da00171aac7843b4e45238418
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-01-21 11:33:32 +0300

    Update 1_About.py

---
+++ pages/1_About.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(page_title="3YON team",
+                   layout='wide',
+                   page_icon='./icons/about.png')
+
+st.markdown("""
+### 3yon Team
+our wonderful team consists of: 
+
+
+    ●   Maseal Alghamdi, PhD
+
+    ●   Abdullah Alshaya
+
+    ●   Abdullah Alzaben
+
+    ●   Anfal AlAwajy
+
+    ●   Nada AlMugren
+
+    ●   Sarah Alghamdi
+
+
+           
+            """)
