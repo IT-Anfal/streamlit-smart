fd29d0a5769341605821721627a708381d15ec6d
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-01-21 11:45:57 +0300

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -4,26 +4,15 @@ st.set_page_config(page_title="3YON team",
                    layout='wide',
                    page_icon='./icons/about.png')
 
-st.subheader("
-3yon Team
-our wonderful team consists of: 
+st.subheader("3yon Team")
+st.title("our wonderful team consists of: ")
+st.write("   ●   Maseal Alghamdi, PhD")
+st.write("   ●   Abdullah Alshaya")
+st.write("   ●   Abdullah Alzaben")
+st.write("   ●   Anfal AlAwajy")
+st.write("   ●   Nada AlMugren")
+st.write("   ●   Sarah Alghamdi")
 
-
-    ●   Maseal Alghamdi, PhD
-
-    ●   Abdullah Alshaya
-
-    ●   Abdullah Alzaben
-
-    ●   Anfal AlAwajy
-
-    ●   Nada AlMugren
-
-    ●   Sarah Alghamdi
-
-
-           
-           ")
 
 st.write("check out our smartathon [page](https://smartathon.hackerearth.com/challenges/hackathon/smartathon-2/dashboard/ae23dcf/team/?utm_source=team-invite&utm_medium=email&utm_campaign=hackathons)")
 
