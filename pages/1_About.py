59860479d6672065e806c530bcd612a2aeed72e4
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-01-21 11:43:25 +0300

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="3YON team",
                    layout='wide',
                    page_icon='./icons/about.png')
 
-st.subheader("""
+st.subheader("
 3yon Team
 our wonderful team consists of: 
 
@@ -23,7 +23,7 @@ our wonderful team consists of:
 
 
            
-            """)
+           ")
 
 st.write("check out our smartathon [page](https://smartathon.hackerearth.com/challenges/hackathon/smartathon-2/dashboard/ae23dcf/team/?utm_source=team-invite&utm_medium=email&utm_campaign=hackathons)")
 
